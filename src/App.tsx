1c5191ad873abda7c9d84ac08da397272c01ebd7
Filip Maćkowiak <Fil.Mackowiak@gmail.com>
2024-06-06 23:03:10 +0200

    feat(86bz3dur0): Added Tanstack Router and set /quest and /representation routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,17 @@
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 import "./App.css";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 function App() {
-  return (
-    <>
-      <Card>
-        <CardHeader>
-          <CardTitle>Work In Progress</CardTitle>
-          <CardDescription>Pełna strona już wkrótce!</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>Witamy!</p>
-        </CardContent>
-        <CardFooter>
-          <p>Zapraszamy na stoisko</p>
-        </CardFooter>
-      </Card>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/routes/quest.tsx
@@ -0,0 +1,5 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/quest')({
+  component: () => <div>Hello /quest!</div>
+})
