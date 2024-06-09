083cdf2f0e2b9f6593546fbc6dcb02c258d63483
Dawid Roszman <dawidroszman@protonmail.com>
2024-06-09 11:51:47 +0200

    feature(86bz44uwa): added base for roadmap

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,89 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { PresentationMode } from "@/features/presentation-mode/presentation-mode.enum";
+import { usePresentationModeContext } from "@/features/presentation-mode/use-presentation-mode.hook";
+import { useEffect, useState } from "react";
+import { Chrono } from "react-chrono";
 
 export default function IndexPage() {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const { presentationMode } = usePresentationModeContext();
+
+  useEffect(() => {
+    const interval = () =>
+      presentationMode === PresentationMode.PRESENTING &&
+      setActiveIndex((activeIndex + 1) % 4);
+    const id = setInterval(interval, 5000);
+    return () => clearInterval(id);
+  }, [activeIndex, presentationMode]);
+  const items = [
+    {
+      title: "May 1940",
+      cardTitle: "Dunkirk",
+      url: "http://www.history.com",
+      cardSubtitle:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      cardDetailedText:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: "http://someurl/image.jpg",
+        },
+      },
+    },
+    {
+      title: "May 1940",
+      cardTitle: "Dunkirk",
+      url: "http://www.history.com",
+      cardSubtitle:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      cardDetailedText:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: "http://someurl/image.jpg",
+        },
+      },
+    },
+    {
+      title: "May 1940",
+      cardTitle: "Dunkirk",
+      url: "http://www.history.com",
+      cardSubtitle:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      cardDetailedText:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: "http://someurl/image.jpg",
+        },
+      },
+    },
+    {
+      title: "May 1940",
+      cardTitle: "Dunkirk",
+      url: "http://www.history.com",
+      cardSubtitle:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      cardDetailedText:
+        "Men of the British Expeditionary Force (BEF) wade out to..",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: "http://someurl/image.jpg",
+        },
+      },
+    },
+  ];
   return (
     <>
-      <Card>
-        <CardHeader>
-          <CardTitle>Work In Progress</CardTitle>
-          <CardDescription>Pełna strona już wkrótce!</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>Witamy!</p>
-        </CardContent>
-        <CardFooter>
-          <p>Zapraszamy na stoisko</p>
-        </CardFooter>
-      </Card>
+      <Chrono
+        items={items}
+        mode="HORIZONTAL"
+        activeItemIndex={activeIndex}
+        disableToolbar
+      />
     </>
   );
 }
